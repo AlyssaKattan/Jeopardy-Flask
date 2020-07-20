27286b90a6565daa927525da4318093aed90146d
Alyssa <alykattan@gmail.com>
2020-07-20 12:11:11 -0400

    initial commit

--- app.py
+++ app.py
@@ -17,4 +17,12 @@ app.secret_key = b'HO\xf8\xff+\n\x1e\\~/;}'
 @app.route('/')
 @app.route('/index')
 def index():
+    session["name"]= "Alyssa"
     return render_template('index.html')
+    
+@app.route("/random")
+def jeopardy_random():
+    #Use jservice API/random to get 1 jeopardy clue
+    response = requests.get('http://jservice.io/api/clues?category=139').json()
+    index= random.randint(0, len(response))
+    return render_template("random_clue.html", data=response[index])
